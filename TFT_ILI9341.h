c0887c17129d417d13c1484345ee8c6affc6d2b6
Bodmer <bodmer@anola.net>
2015-12-01 00:57:40 +0000

    Add color list for backwards compatibility
    Correct Setup reference
    Add begin() for backwards compatibility

--- TFT_ILI9341.h
+++ TFT_ILI9341.h
@@ -16,7 +16,7 @@
  ****************************************************/
 
 // Include header file that defines the fonts loaded and the pins to be used
-#include <User_Setup2.h>
+#include <User_Setup.h>
 
 // Include header file from the FastLED library for fast pin toggling using direct port access
 #include <TFT_FastPin.h>
@@ -162,7 +162,7 @@ swap(T& a, T& b) { T t = a; a = b; b = t; }
 #define ILI9341_MADCTL_BGR 0x08
 #define ILI9341_MADCTL_MH  0x04
 
-// Color definitions
+// New color definitions use for all my libraries
 #define TFT_BLACK       0x0000      /*   0,   0,   0 */
 #define TFT_NAVY        0x000F      /*   0,   0, 128 */
 #define TFT_DARKGREEN   0x03E0      /*   0, 128,   0 */
@@ -182,6 +182,27 @@ swap(T& a, T& b) { T t = a; a = b; b = t; }
 #define TFT_ORANGE      0xFD20      /* 255, 165,   0 */
 #define TFT_GREENYELLOW 0xAFE5      /* 173, 255,  47 */
 #define TFT_PINK        0xF81F
+
+// Color definitions for backwards compatibility
+#define ILI9341_BLACK       0x0000      /*   0,   0,   0 */
+#define ILI9341_NAVY        0x000F      /*   0,   0, 128 */
+#define ILI9341_DARKGREEN   0x03E0      /*   0, 128,   0 */
+#define ILI9341_DARKCYAN    0x03EF      /*   0, 128, 128 */
+#define ILI9341_MAROON      0x7800      /* 128,   0,   0 */
+#define ILI9341_PURPLE      0x780F      /* 128,   0, 128 */
+#define ILI9341_OLIVE       0x7BE0      /* 128, 128,   0 */
+#define ILI9341_LIGHTGREY   0xC618      /* 192, 192, 192 */
+#define ILI9341_DARKGREY    0x7BEF      /* 128, 128, 128 */
+#define ILI9341_BLUE        0x001F      /*   0,   0, 255 */
+#define ILI9341_GREEN       0x07E0      /*   0, 255,   0 */
+#define ILI9341_CYAN        0x07FF      /*   0, 255, 255 */
+#define ILI9341_RED         0xF800      /* 255,   0,   0 */
+#define ILI9341_MAGENTA     0xF81F      /* 255,   0, 255 */
+#define ILI9341_YELLOW      0xFFE0      /* 255, 255,   0 */
+#define ILI9341_WHITE       0xFFFF      /* 255, 255, 255 */
+#define ILI9341_ORANGE      0xFD20      /* 255, 165,   0 */
+#define ILI9341_GREENYELLOW 0xAFE5      /* 173, 255,  47 */
+#define ILI9341_PINK        0xF81F
 
 typedef struct {
 	const unsigned char *chartbl;
@@ -240,7 +261,7 @@ class TFT_ILI9341 : public Print {
 
   TFT_ILI9341(int16_t _W = ILI9341_TFTWIDTH, int16_t _H = ILI9341_TFTHEIGHT);
 
-  void     init(void),
+  void     init(void), begin(void), // Same - begin included for backwards compatibility
 
            drawPixel(uint16_t x, uint16_t y, uint16_t color),
 
